be913431066ba1e5a17fca505c56d23e9d9456c6
Lawrence Daniels <lawrence.daniels@gmail.com>
2017-02-04 16:22:49 -0800

    first commit

---
+++ src/main/scala/io/scalajs/npm/mongodb/Connection.scala
@@ -0,0 +1,12 @@
+package io.scalajs.npm.mongodb
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+/**
+  * MongoDB Connection
+  * @author lawrence.daniels@gmail.com
+  */
+@js.native
+@JSImport("mongodb", "Connection")
+class Connection extends js.Object {}
---
+++ src/test/scala/io/scalajs/npm/mongodb/GridFSBucketTest.scala
@@ -0,0 +1,33 @@
+package io.scalajs.npm.mongodb
+
+import io.scalajs.nodejs.fs.Fs
+import io.scalajs.npm.mongodb.gridfs.GridFSBucket
+import org.scalatest.FunSpec
+
+/**
+  * GridFSBucket Test
+  * @author lawrence.daniels@gmail.com
+  */
+class GridFSBucketTest extends FunSpec {
+  val url = "mongodb://localhost:27017/test"
+
+  describe("GridFSBucket") {
+
+    it("supports streaming files to disk") {
+      // TODO either mock or use an embedded server
+      /*
+      MongoClient.connect(
+        url,
+        (err, db) => {
+          assert(null == err, err)
+
+          val bucket = new GridFSBucket(db)
+          Fs.createReadStream(Fs.realpathSync("./src/test/resources/kermit-yoda.jpeg"))
+            .pipe(bucket.openUploadStream("kermit+yoda.jpeg"))
+            .on("error", (error: MongoError) => assert(null == error, error))
+        })*/
+    }
+
+  }
+
+}
